58a78d6a3844b9158f7fc8f5cfd4609aa2d44a51
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-07-15 22:51:02 -0500

    Update Lambda function to send message to SQS.py

--- Lambda function to send message to SQS.py
+++ Lambda function to send message to SQS.py
@@ -1,10 +1,11 @@
 import json
 import boto3
-
-def lambda_handler(event,context)
 from datetime import datetime
-
+def lambda_handler(event,context)
 now = datetime.now()
+client = boto3.client("sqs")
+#input Queue URL the URL that provided when you created the SQS queue
+response = client.send_message(QueueUrl-,MessageBody= event[
 
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
