1764b4bfb97d408624c6016dcf8b8492535b0cfd
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-07-17 21:48:30 -0500

    Update Lambda function to send message to SQS.py

--- Lambda function to send message to SQS.py
+++ Lambda function to send message to SQS.py
@@ -1,11 +1,16 @@
 import json
 import boto3
 from datetime import datetime
-def lambda_handler(event,context)
-now = datetime.now()
-client = boto3.client("sqs")
-#input Queue URL the URL that provided when you created the SQS queue
-response = client.send_message(QueueUrl=https://sqs.us-east-1.amazonaws.com/767779967756/time,MessageBody= event[
 
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
+def lambda_handler(event,context)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    sqs = boto3.client("sqs")
+    sqs.send_message(
+    #input Queue URL the URL that provided when you created the SQS queue
+         response = client.send_message(QueueUrl=https://sqs.us-east-1.amazonaws.com/767779967756/time,MessageBody= current_time
+        )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(current_time)
+    }
